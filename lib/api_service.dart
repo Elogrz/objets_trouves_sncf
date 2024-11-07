d5556730246ca61e0e5e6febd0badea1ce6bdc06
Elogrz <eloise.26@hotmail.fr>
2024-11-07 15:25:53 +0100

    selection et requetes
    selection de gares et de catégories d'objets avec menu déroulant / test bdd réduit ok

---
+++ lib/api_service.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String apiUrl = 'https://data.sncf.com/api/records/1.0/search/?dataset=objets-trouves-restitution';
+
+  Future<List<dynamic>> fetchLostItems({String? gare, String? typeObject}) async {
+    final uri = Uri.parse(apiUrl + '&q=${gare ?? ''}+${typeObject ?? ''}');
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      return data['records'] ?? [];
+    } else {
+      throw Exception('Erreur lors de la récupération des données');
+    }
+  }
+
+  Future<List<String>> fetchAllGares() async {
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+
+      Set<String> gares = {};
+      for (var record in data['records']) {
+        if (record['fields']['gc_obo_gare_origine_r_name'] != null) {
+          gares.add(record['fields']['gc_obo_gare_origine_r_name']);
+        }
+      }
+
+      return gares.toList();
+    } else {
+      throw Exception('Erreur lors de la récupération des gares');
+    }
+  }
+
+  Future<List<String>> fetchAllTypeObject() async {
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+
+      Set<String> typeObject = {};
+      for (var record in data['records']) {
+        if (record['fields']['gc_obo_type_c'] != null) {
+          typeObject.add(record['fields']['gc_obo_type_c']);
+        }
+      }
+
+      return typeObject.toList();
+    } else {
+      throw Exception('Erreur lors de la récupération des gares');
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> fetchFilteredObjects({
+    required String gare,
+    required String typeObject,
+  }) async {
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+
+      List<Map<String, dynamic>> filteredItems = [];
+      for (var record in data['records']) {
+        var fields = record['fields'];
+        if (fields['gc_obo_gare_origine_r_name'] == gare &&
+            fields['gc_obo_type_c'] == typeObject) {
+          filteredItems.add(fields);
+        }
+      }
+
+      return filteredItems;
+    } else {
+      throw Exception('Erreur lors de la récupération des objets filtrés');
+    }
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,160 @@
 import 'package:flutter/material.dart';
+import 'api_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SNCF Connect',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  final ApiService apiService = ApiService();
+  String? selectedGare;
+  String? selectedTypeObject;
+  Future<List<dynamic>>? _futureLostItems;
+  List<String> gares = [];
+  List<String> typeObjects = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    loadGares();
+    loadTypeObject();  // Assurez-vous que cette fonction est appelée ici
+  }
+
+  Future<void> loadGares() async {
+    try {
+      final fetchedGares = await apiService.fetchAllGares();
+      setState(() {
+        gares = fetchedGares;
+      });
+    } catch (e) {
+      print('Erreur lors de la récupération des gares : $e');
+    }
+  }
+
+  Future<void> loadTypeObject() async {
+    try {
+      final fetchedTypeObjects = await apiService.fetchAllTypeObject();
+      setState(() {
+        typeObjects = fetchedTypeObjects;
+      });
+    } catch (e) {
+      print('Erreur lors de la récupération des types d\'objets : $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('SNCF Connect'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            if (gares.isNotEmpty) // Affiche le menu déroulant des gares seulement si les données sont prêtes
+              DropdownButton<String>(
+                hint: Text('Sélectionnez la gare'),
+                value: selectedGare,
+                isExpanded: true,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedGare = newValue;
+                  });
+                },
+                items: gares.map((gare) {
+                  return DropdownMenuItem(
+                    value: gare,
+                    child: Text(gare),
+                  );
+                }).toList(),
+              ),
+            if (typeObjects.isNotEmpty) // Affiche le menu déroulant des types seulement si les données sont prêtes
+              DropdownButton<String>(
+                hint: Text('Sélectionnez la catégorie'),
+                value: selectedTypeObject,
+                isExpanded: true,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedTypeObject = newValue;
+                  });
+                },
+                items: typeObjects.map((typeObject) {
+                  return DropdownMenuItem(
+                    value: typeObject,
+                    child: Text(typeObject),
+                  );
+                }).toList(),
+              ),
+            ElevatedButton(
+              onPressed: () {
+                if (selectedGare != null && selectedTypeObject != null) {
+                  print('Gare sélectionnée: $selectedGare'); // Débogage
+                  print('Type d\'objet sélectionné: $selectedTypeObject'); // Débogage
+                  setState(() {
+                    _futureLostItems = apiService.fetchFilteredObjects(
+                      gare: selectedGare!,
+                      typeObject: selectedTypeObject!,
+                    );
+                  });
+                } else {
+                  print('Veuillez sélectionner une gare et un type d\'objet');
+                }
+              },
+              child: Text('Rechercher'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              child: FutureBuilder<List<dynamic>>(
+                future: _futureLostItems,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Erreur : ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(child: Text('Aucun objet trouvé.'));
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        final item = snapshot.data![index];
+                        return Card(
+                          child: ListTile(
+                            title: Text(item['gc_obo_nature_c'] ?? 'N/A'),
+                            subtitle: Text(
+                              'Gare: ${item['gc_obo_gare_origine_r_name'] ?? 'N/A'}\n'
+                                  'Date: ${item['date'] ?? 'N/A'}',
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
